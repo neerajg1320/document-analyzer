f2f04e323b741da84b8dd66c3c4573497db9c8d2
Neeraj Gupta <neeraj@finnirv.com>
2019-06-13 11:20:49 +0530

    Added CORS acceptance. We are using django-cors-middleware library

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',  # This is required for Token model
     'core',
@@ -44,9 +45,12 @@ INSTALLED_APPS = [
     'extractor',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
