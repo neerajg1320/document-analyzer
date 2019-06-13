477c03bcb04d8d1bb91e5e02088199658b519768
Neeraj Gupta <neeraj@finnirv.com>
2019-06-13 14:03:08 +0530

    Added http test commands

--- http-commands/http_public_user_create.sh
+++ http-commands/http_public_user_create.sh
@@ -7,7 +7,7 @@ then
   FLAGS="${FLAGS} --debug"
 fi
 
-http ${FLAGS} --json POST http://localhost:8000/api/user/create/ <<< '{ "email": "alice@abc.com", "name": "Alice", "password": "Alice123" }'
+http ${FLAGS} --json POST http://localhost:8000/api/user/create/ <<< '{ "email": "bob@abc.com", "name": "Bob", "password": "Bob123" }'
 
 # The following also works
 # echo '{ "email": "alice@abc.com", "name": "Alice", "password": "Alice123" }' | http --debug --json POST http://localhost:8000/api/user/create/ 
