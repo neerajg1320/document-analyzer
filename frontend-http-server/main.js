cac4f2f60b7577e6ee751809685d86244ed7c535
Neeraj Gupta <neeraj@finnirv.com>
2019-06-13 15:40:59 +0530

    Created a scrollable text box the the document text which we show below the transactions table

--- frontend-http-server/main.js
+++ frontend-http-server/main.js
@@ -1,5 +1,5 @@
 //create Tabulator on DOM element with id "example-table"
-let g_tabulator_table = new Tabulator("#transactions-table", {
+let g_tabulator_table = new Tabulator("#document-transactions-table", {
     height:300,
     layout:"fitColumns", //fit columns to width of table (optional)
     autoColumns:true,
@@ -9,6 +9,7 @@ let g_tabulator_table = new Tabulator("#transactions-table", {
     },
 });
 
+let g_document_text_box = $("#document-text")
 
 let g_user_auth_token = '864556deab19c0b84c28d1a56b20c2c3808c9099';
 
@@ -19,23 +20,38 @@ function set_sample_transactions(tabulator_table) {
     tabulator_table.setData(credit_card_data);
 }
 
-function download_document_transactions(tabulator_table, document_id, user_auth_token) {
+function download_document_transactions(user_auth_token, document_id, tabulator_table, elm_text_box) {
     if (document_id == "") {
         alert("Please provide a valid document Id");
         return;
     }
 
-    // http://localhost:8000/api/docminer/documents/<document_id>/transactions/json/
-    let document_json_url = 'http://localhost:8000/api/docminer/documents/' + document_id + '/transactions/json/';
+    // http://localhost:8000/api/docminer/documents/<document_id>/
+    let document_url = 'http://localhost:8000/api/docminer/documents/' + document_id + '/';
 
     $.ajax({
-        url: document_json_url,
+        url: document_url,
+        headers : {
+            'Authorization' : 'Token ' + user_auth_token,
+        },
+        dataType: 'json',
+        success: function(document) {
+            // console.log(typeof(document), document);
+            elm_text_box.empty().append(document.text);
+        }
+    });
+
+    // http://localhost:8000/api/docminer/documents/<document_id>/transactions/json/
+    let document_transactions_json_url = 'http://localhost:8000/api/docminer/documents/' + document_id + '/transactions/json/';
+
+    $.ajax({
+        url: document_transactions_json_url,
         headers : {
             'Authorization' : 'Token ' + user_auth_token,
         },
         dataType: 'json',
         success: function(response) {
-            console.log(typeof(response), response);
+            // console.log(typeof(response), response);
             //response is already a parsed JSON
 
             tabulator_table.setData(response);
@@ -49,7 +65,7 @@ function download_document_transactions(tabulator_table, document_id, user_auth_
 function download_document_using_input(tabulator_table, user_auth_token) {
     document_id = $("#input_document_id").val();
 
-    download_document_transactions(tabulator_table, document_id, user_auth_token)
+    download_document_transactions(user_auth_token, document_id, tabulator_table, g_document_text_box)
 }
 
 
