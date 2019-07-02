e7bb08d1c6f4d2ebc748607cf6fe0e9b5528a71a
Neeraj Gupta <neeraj@finnirv.com>
2019-07-02 13:08:33 +0530

    Show the transactions in the frontend

--- frontend-http-server/main.js
+++ frontend-http-server/main.js
@@ -28,9 +28,9 @@ let g_document_pandas_box = $("#document-pandas");
 // MacPro Docker
 let g_user_auth_token_docker = '307e60bcf5f1930b39a6ce5bc87b171ed0451323';
 // MacPro Local
-let g_user_auth_token_local = '219201bc10fb6baa4a4cbc36d318aedaa89f78b7';
+// let g_user_auth_token_local = '219201bc10fb6baa4a4cbc36d318aedaa89f78b7';
 // MacAir Local
-// let g_user_auth_token_local = '0676010d893a1e1cd15f5d8a3883b5ced174fdad';
+let g_user_auth_token_local = '0676010d893a1e1cd15f5d8a3883b5ced174fdad';
 
 
 
@@ -184,6 +184,9 @@ $("#btn_create_regex").click(function() {
             // g_account_table.setData(response);
             var new_str = response[0]['new_str'];
             g_document_text_box.empty().append(new_str);
+
+            var transactions = response[0]['transactions'];
+            g_account_table.setData(transactions);
         }
     });
 
