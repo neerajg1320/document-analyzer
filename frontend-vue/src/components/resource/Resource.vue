d083da9a5c1fa43cf1e7fa85108d307faba987aa
Neeraj Gupta <neeraj@finnirv.com>
2019-08-26 13:39:16 +0530

    Cleaned up the Resource.vue file

--- frontend-vue/src/components/resource/Resource.vue
+++ frontend-vue/src/components/resource/Resource.vue
@@ -48,24 +48,26 @@
 </template>
 
 <script>
-  // import {USER_REQUEST} from '../../store/actions/user'
   import { mapActions, mapGetters } from 'vuex';
-  const resource_name = 'operations';
-
-  const prvFetchResourceFromPath = function (path, component) {
+  
+  function prvGetResourceNameFromPath(path) {
     const path_split_array = path.split('/');
 
-    var resource_name_from_path = "none";
+    var resource_name = "none";
     if (path_split_array.length > 2) {
-      resource_name_from_path = path_split_array[2];
+      resource_name = path_split_array[2];
     }
 
+    return resource_name;
+  }
+
+  function prvFetchResources (resource_name, component) {
     component.loading = true;
     // eslint-disable-next-line
     component.userRequest().then(resp => {
       // console.log(resp);
     });
-    const payload = {'resource_name': resource_name_from_path};
+    const payload = {'resource_name': resource_name};
 
     component.fetchResources(payload)
         .then(resp => {
@@ -82,6 +84,7 @@
     name: "Resources",
     data () {
       return {
+        'resource_name': 'none',
         loading: false,
         model: {
           type: 'Extract',
@@ -107,7 +110,10 @@
       },
 
       async addInstance() {
-        const payload = {resource_name, "instance": this.model}
+        const payload = {
+          "resource_name": this.resource_name,
+          "instance": this.model
+        };
 
         if (this.model.id) {
           await this.updateResource(payload)
@@ -120,7 +126,10 @@
 
       async deleteInstance (id) {
         if (confirm('Are you sure you want to delete?')) {
-          const payload = {resource_name, id};
+          const payload = {
+            "resource_name": this.resource_name,
+            id
+          };
           await this.delResource(payload)
         }
       }
@@ -129,13 +138,17 @@
     // Resource and Profile have a shared creation code
     async created() {
       console.log("created:", this.$route.path);
-      prvFetchResourceFromPath(this.$route.path, this);
+      const resource_name = prvGetResourceNameFromPath(this.$route.path);
+      this.resource_name = resource_name;
+      prvFetchResources(resource_name, this);
     },
 
     // https://stackoverflow.com/questions/43461882/update-vuejs-component-on-route-change
     async beforeRouteUpdate (to, from, next) {
       console.log("beforeRouteUpdate:", to.path);
-      prvFetchResourceFromPath(to.path, this);
+      const resource_name = prvGetResourceNameFromPath(to.path);
+      this.resource_name = resource_name;
+      prvFetchResources(resource_name, this);
       next();
     },
   }
