45ff5cbd72031c6fdfc72bcba4e91122da724f02
Neeraj Gupta <neeraj@finnirv.com>
2019-08-26 21:41:56 +0530

    created a resource data property from a getter

--- frontend-vue/src/components/resource/ResourceDetail.vue
+++ frontend-vue/src/components/resource/ResourceDetail.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <!-- Form for new resource -->
-        <b-card :title="(instance.id ? 'Edit ' + currentResource + ' ID#' + instance.id : 'New ' + currentResource )">
+        <b-card :title="(instance.id ? 'Edit ' + resource + ' ID#' + instance.id : 'New ' + resource )">
             <form @submit.prevent="saveInstance">
                 <b-form-group label="Title">
                     <b-form-input type="text" v-model="instance.title"></b-form-input>
@@ -29,21 +29,13 @@
           type: 'Extract',
           parameters: "None"
         },
-        instance: {}
+        instance: {},
+        resource: ''
       }
     },
-    computed: mapGetters(['currentResource', 'currentInstance']),
 
-    watch: {
-      currentInstance(newValue, oldValue) {
-        console.log(`Updating instance to  ${newValue.id} from ${oldValue.id}`);
-        this.instance  = Object.assign({}, this.currentInstance);
-      },
-
-      currentResource(newValue, oldValue) {
-        console.log(`Updating resource to '${newValue}' from '${oldValue}'`);
-        this.instance  = Object.assign({}, this.instanceInitState);
-      },
+    computed: {
+      ...mapGetters(['currentResource', 'currentInstance']),
     },
 
     methods: {
@@ -73,8 +65,24 @@
       },
     },
 
+    watch: {
+      currentInstance(newValue, oldValue) {
+        // eslint-disable-next-line
+        console.log(`Updating instance to  ${newValue.id} from ${oldValue.id}`);
+        this.instance  = Object.assign({}, this.currentInstance);
+      },
+
+      currentResource(newValue, oldValue) {
+        // eslint-disable-next-line
+        console.log(`Updating resource to '${newValue}' from '${oldValue}'`);
+        this.resource = newValue;
+        this.instance  = Object.assign({}, this.instanceInitState);
+      },
+    },
+
     created() {
       this.resetInstance();
+      this.resource = this.currentResource;
     }
   }
 </script>
