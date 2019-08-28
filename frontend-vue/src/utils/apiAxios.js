d25995226c2bd0d13cdabfa551ebff78c99722ca
Neeraj Gupta <neeraj@finnirv.com>
2019-08-28 11:33:36 +0530

    Changed apiDocMiner to apiAxios as it can handle any client now

--- frontend-vue/src/utils/apiAxios.js
+++ frontend-vue/src/utils/apiAxios.js
@@ -1,17 +1,8 @@
-import axios from "axios";
-
 const debug = true;
-
-const baseRequest = {
-  baseURL: 'http://localhost:8000/api/docminer/',
-  json: true
-};
-
-const client = axios.create(baseRequest);
 
 export default {
 
-  async execute ({ method, url, auth_token, data }) {
+  async execute ({ client, method, url, auth_token, data }) {
     // When you authenticate with OIDC, an access token is persisted locally
     // in the browser.
     // Inject the accessToken for each request
--- frontend-vue/src/utils/apiResource.js
+++ frontend-vue/src/utils/apiResource.js
@@ -1,24 +1,33 @@
-import apiDocMiner from './apiDocMiner';
+import axios from "axios";
+import apiDocMiner from './apiAxios';
+
+
+const baseRequest = {
+  baseURL: 'http://localhost:8000/api/docminer/',
+  json: true
+};
+
+const client = axios.create(baseRequest);
 
 export default {
 
   getList (resource, token) {
-    return apiDocMiner.execute({method:'get', url:`/${resource}/`, token})
+    return apiDocMiner.execute({client, method:'get', url:`/${resource}/`, token})
   },
 
   get (resource, token, id) {
-    return apiDocMiner.execute({method:'get', url:`/${resource}/${id}/`, token})
+    return apiDocMiner.execute({client, method:'get', url:`/${resource}/${id}/`, token})
   },
   post (resource, token, data) {
-    return apiDocMiner.execute({method:'post', url:`/${resource}/`, token, data})
+    return apiDocMiner.execute({client, method:'post', url:`/${resource}/`, token, data})
   },
   put (resource, token, id, data) {
-    return apiDocMiner.execute({method:'put', url:`/${resource}/${id}/`, token, data})
+    return apiDocMiner.execute({client, method:'put', url:`/${resource}/${id}/`, token, data})
   },
   patch (resource, token, id, data) {
-    return apiDocMiner.execute({method:'patch', url:`/${resource}/${id}/`, token, data})
+    return apiDocMiner.execute({client, method:'patch', url:`/${resource}/${id}/`, token, data})
   },
   del (resource, token, id) {
-    return apiDocMiner.execute({method:'delete', url:`/${resource}/${id}/`, token})
+    return apiDocMiner.execute({client, method:'delete', url:`/${resource}/${id}/`, token})
   }
 }
