6dd8d4be6caaec2257c83311ab99ecd133ec3897
Neeraj Gupta <neeraj@finnirv.com>
2019-08-28 11:19:29 +0530

    Modularized the apiDocMiner code

--- frontend-vue/src/utils/apiDocMiner.js
+++ frontend-vue/src/utils/apiDocMiner.js
@@ -11,7 +11,7 @@ const client = axios.create(baseRequest);
 
 export default {
 
-  async execute (method, url, token, data) {
+  async execute (method, url, auth_token, data) {
     // When you authenticate with OIDC, an access token is persisted locally
     // in the browser.
     // Inject the accessToken for each request
@@ -19,11 +19,20 @@ export default {
     const request = {
       method,
       url: url,
-      data,
-      headers: {
-        Authorization: `token ${token}`
-      }
     };
+
+    if (data) {
+      request.data = data;
+    }
+
+    const headers = {};
+    if (auth_token) {
+      headers.Authorization = `token ${auth_token}`;
+    }
+
+    if (headers.length > 0) {
+      request.headers = headers;
+    }
 
     if (debug) {
       console.log("request:", request);
